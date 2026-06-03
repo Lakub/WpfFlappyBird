81610d46c0ef2efafd108991fb6190cd1b73fdbd
Lakub <kuba.loszewski@op.pl>
2026-06-03 13:56:05 +0200

    Added pipe spawning and bird rotation

--- FlappyBirdClone/FlappyBirdWindow.xaml.cs
+++ FlappyBirdClone/FlappyBirdWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,8 +13,10 @@ using System.Windows.Input;
 using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace FlappyBirdClone
 {
@@ -38,14 +43,18 @@ namespace FlappyBirdClone
     /// </summary>
     public partial class FlappyBirdWindow : Window
     {
+        public static Random random;
         Stopwatch sw;
         Size originalSize;
         static public Canvas GameCanvas;
-        static public Bird bird;
-        static public List<GameElement> pipes;
+        public Bird bird;
+        static public Pipe templatePipe;
+        static public List<Pipe> pipes;
+        static public int points;
         public FlappyBirdWindow()
         {
             InitializeComponent();
+            random = new();
             pipes = new();
             originalSize = new Size(this.Width, Height);
             GameCanvas = GameScreen;
@@ -53,6 +62,8 @@ namespace FlappyBirdClone
             sw = new();
             sw.Start();
             bird = new Bird(BirdSprite);
+            templatePipe = new Pipe(PipeSprite,false);
+            templatePipe.enabled = false;
             CompositionTarget.Rendering += UpdateGameElements;
             SizeChanged += ResizeCanvas;
             KeyDown += AlertBird;
@@ -70,7 +81,6 @@ namespace FlappyBirdClone
             if (diffWidth > diffHeight)
             {
                 scaleVal = diffHeight;
-
             }
             else
             {
@@ -79,10 +89,25 @@ namespace FlappyBirdClone
             scale.ScaleX = scaleVal;
             scale.ScaleY = scaleVal;
         }
+        void CreatePipe()
+        {
+            var pipe = pipes.Find(e => !e.enabled);
+            if (pipe == null)
+                templatePipe.DeepCopy();
+            else
+                pipe.ResetPosition();
+        }
+        double counter=0;
         public void UpdateGameElements(object sender,EventArgs e)
         {
             Time.deltaSeconds = sw.Elapsed.TotalSeconds;
             sw.Restart();
+            counter += Time.deltaSeconds;
+            if (counter >= 3.8)
+            {
+                counter = 0;
+                CreatePipe();
+            }
             bird.Update();
             foreach (var element in pipes)
             {
@@ -108,7 +133,15 @@ namespace FlappyBirdClone
             Canvas.SetTop(uiElement, Canvas.GetTop(uiElement) - y);
         }
         abstract public void Update();
-
+        protected UIElement CopyElement()
+        {
+            string saved = XamlWriter.Save(uiElement);
+            string pattern = "Name\\s*=\\s*\"([a-zA-Z]*|\\s*|\\d*)\"\\s";
+            var element = XamlReader.Parse(Regex.Replace(saved, pattern, String.Empty));
+            FlappyBirdWindow.GameCanvas.Children.Add(element as UIElement);
+            return element as UIElement;
+        }
+        
         public abstract GameElement DeepCopy();
         public static void CreateCopy(GameElement gameElement)
         {
@@ -116,23 +149,100 @@ namespace FlappyBirdClone
             FlappyBirdWindow.GameCanvas.Children.Add(gameElement.uiElement);
         }
     }
+
+    public class Pipe : GameElement
+    {
+        static int minY = 131;
+        static int maxY = 426;
+        static int minYDistance = -701;
+        static int maxYDistance = -615;
+        static int startingX = 805;
+        static int endingX = -105;
+
+        Pipe topPipe;
+        public bool enabled
+        {
+            get
+            {
+                return uiElement.Visibility == Visibility.Visible;
+            }
+            set
+            {
+                if (value)
+                    uiElement.Visibility = Visibility.Visible;
+                else
+                    uiElement.Visibility = Visibility.Collapsed;
+            }
+        }
+        public Pipe(UIElement uiElement, bool enabled=true) : base(uiElement)
+        {
+            if(enabled){
+                FlappyBirdWindow.pipes.Add(this);
+                topPipe = CreateTopPipe();
+                ResetPosition();
+            }
+        }
+        public Pipe(UIElement uiElement, int dum) : base(uiElement) { }
+
+        public void ResetPosition()
+        {
+            enabled = true;
+            Canvas.SetLeft(uiElement, startingX);
+            var myY = FlappyBirdWindow.random.Next(minY, maxY + 1);
+            Canvas.SetTop(uiElement, myY);
+            topPipe.ResetTopPosition(myY);
+        }
+        public void ResetTopPosition(int botY)
+        {
+            enabled = true;
+            Canvas.SetLeft(uiElement, startingX);
+            var pos = botY + FlappyBirdWindow.random.Next(minYDistance, maxYDistance + 1);
+            Trace.WriteLine(pos);
+            Canvas.SetTop(uiElement, pos);
+        }
+
+        public Pipe CreateTopPipe()
+        {
+            return new Pipe(CopyElement(),0);
+        }
+
+        public override GameElement DeepCopy()
+        {
+            return new Pipe(CopyElement());
+        }
+
+        public override void Update()
+        {
+            if(!enabled) return;
+            if(topPipe!=null)
+                topPipe.Update();
+            Move(-140*Time.deltaSeconds, 0);
+            if (Canvas.GetLeft(uiElement) <= endingX)
+                enabled = false;
+        }
+    }
     public class Bird : GameElement
     {
         bool jumpPressed;
         double verticalAcceleration;
+        RotateTransform rotateTransform;
         public Bird(UIElement uiElement) : base(uiElement)
         {
+            rotateTransform = uiElement.RenderTransform as RotateTransform;
         }
         public override void Update()
         {
             if (jumpPressed)
             {
-                verticalAcceleration = 100;
+                verticalAcceleration = 200;
             }
             else
             {
-                verticalAcceleration -= 122*Time.deltaSeconds;
+                verticalAcceleration -= 250*Time.deltaSeconds;
             }
+
+            rotateTransform.Angle += -verticalAcceleration * 5 * Time.deltaSeconds;
+            rotateTransform.Angle=Math.Clamp(rotateTransform.Angle, -30, 30);
             Move(0, verticalAcceleration * Time.deltaSeconds);
             jumpPressed = false;
         }
@@ -143,9 +253,8 @@ namespace FlappyBirdClone
         }
         public override GameElement DeepCopy()
         {
-            string saved = XamlWriter.Save(uiElement);
-            var element = XamlReader.Parse(saved);
-            return new Bird((UIElement)element);
+            
+            return new Bird(CopyElement());
         }
     }
 }
